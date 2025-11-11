eef236334137ae248d93d8e5d3d27ed4f2904f10
Nolan Brown <noleetarrr@gmail.com>
2025-11-11 01:55:07 -0800

    undo videoplayer edits

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react"
+import React from "react"
 import ReactPlayer from "react-player"
 
 const VideoPlayer = (props) => {
@@ -9,23 +9,7 @@ const VideoPlayer = (props) => {
     },
   }
 
-  const playerRef = useRef(null)
-
-  useEffect(() => {
-    const player = playerRef.current ? playerRef.current.getInternalPlayer() : null;
-    if (player) {
-      if (player.tagName === 'IFRAME') {
-        player.setAttribute('referrerpolicy', 'strict-origin-when-cross-origin');
-      } else {
-        const iframe = player.querySelector('iframe');
-        if (iframe) {
-          iframe.setAttribute('referrerpolicy', 'strict-origin-when-cross-origin');
-        }
-      }
-    }
-  }, [props.src]);
-
-  return <ReactPlayer ref={playerRef} config={config} controls {...props} />
+  return <ReactPlayer config={config} controls {...props} />
 }
 
 export default VideoPlayer
